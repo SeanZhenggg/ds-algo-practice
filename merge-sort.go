c1e4042e5b65ebad2bcb44b9bb47a20d7cac0485
seanZhenggg <s410585038@gmail.com>
2024-11-13 23:01:54 +0800

    add merge sort

---
+++ merge-sort.go
@@ -0,0 +1,56 @@
+package main
+
+import "log"
+
+func MergeSort(nums []int, l int, r int) []int {
+	// 拆分左右兩邊，繼續遞迴處理左半邊跟右半邊
+
+	if l == r {
+		return []int{nums[l]}
+	}
+
+	mid := l + (r-l)/2
+
+	left := MergeSort(nums, l, mid)
+	right := MergeSort(nums, mid+1, r)
+
+	merge(nums, left, right, l, r)
+	arr := make([]int, r-l+1)
+	copy(arr, nums[l:r+1])
+	return arr
+}
+
+func merge(nums []int, left []int, right []int, l int, r int) {
+	var ll = 0
+	var rr = 0
+
+	for ll < len(left) && rr < len(right) {
+		if left[ll] <= right[rr] {
+			nums[l] = left[ll]
+			ll++
+		} else {
+			nums[l] = right[rr]
+			rr++
+		}
+		l++
+	}
+
+	if ll < len(left) {
+		for i := ll; i < len(left); i++ {
+			nums[l] = left[i]
+			l++
+		}
+	}
+	if rr < len(right) {
+		for i := rr; i < len(right); i++ {
+			nums[l] = right[i]
+			l++
+		}
+	}
+}
+
+func main() {
+	nums := []int{3, 1, 5, 2, 4}
+	MergeSort(nums, 0, 4)
+	log.Printf("nums: %v", nums)
+}

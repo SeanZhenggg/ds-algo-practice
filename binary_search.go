988c33614d205f261c95a55a310febac8eafcc90
seanZhenggg <s410585038@gmail.com>
2024-12-25 00:29:10 +0800

    update bs

--- binary_search.go
+++ binary_search.go
@@ -7,17 +7,21 @@ import (
 func main() {
 
 	arr := []int{1, 4, 7, 10, 13}
-	search := 13
-	idx, found := binarySearch(arr, search)
-	if found {
-		var val int
-		if idx != -1 {
-			val = arr[idx]
-		}
-		log.Printf("search value: %d, index: %d, the corresponding value is: %d", search, idx, val)
-	} else {
-		log.Printf("value containing the search value not found")
-	}
+	idx, found := binarySearch(arr, 13)
+	log.Printf("found: %t, at index: %d", found, idx)
+
+	idx, found = binarySearchWithFindGreaterMinIndexWhenNotFound(arr, 8)
+	log.Printf("found: %t, at index: %d", found, idx)
+
+	idx, found = binarySearchWithFindLesserMaxIndexWhenNotFound(arr, 8)
+	log.Printf("found: %t, at index: %d", found, idx)
+
+	arr2 := []int{1, 4, 7, 7, 10, 13}
+	idx, found = binarySearchWithDuplicateSearchValues(arr2, 7)
+	log.Printf("found: %t, at index: %d", found, idx)
+
+	idx, found = binarySearchWithDuplicateSearchValues2(arr2, 7)
+	log.Printf("found: %t, at index: %d", found, idx)
 }
 
 func binarySearch(arr []int, search int) (int, bool) {
@@ -25,20 +29,98 @@ func binarySearch(arr []int, search int) (int, bool) {
 		return -1, false
 	}
 
-	var left, right, mid int
-	left = 0
-	right = len(arr) - 1
+	l, r := 0, len(arr)-1
 
-	for left <= right {
-		mid = (left + right) / 2
+	for l <= r {
+		mid := (l + r) / 2
 		if search < arr[mid] {
-			right = mid - 1
+			r = mid - 1
 		} else if search == arr[mid] {
 			return mid, true
 		} else {
-			left = mid + 1
+			l = mid + 1
 		}
 	}
 
 	return -1, false
+}
+
+func binarySearchWithFindGreaterMinIndexWhenNotFound(arr []int, search int) (int, bool) {
+	if len(arr) == 0 {
+		return -1, false
+	}
+
+	l, r := 0, len(arr)-1
+
+	for l <= r {
+		mid := (l + r) / 2
+		if search < arr[mid] {
+			r = mid - 1
+		} else if search == arr[mid] {
+			return mid, true
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return l, true
+}
+
+func binarySearchWithFindLesserMaxIndexWhenNotFound(arr []int, search int) (int, bool) {
+	if len(arr) == 0 {
+		return -1, false
+	}
+
+	l, r := 0, len(arr)-1
+
+	for l <= r {
+		mid := (l + r) / 2
+		if search < arr[mid] {
+			r = mid - 1
+		} else if search == arr[mid] {
+			return mid, true
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return r, true
+}
+
+func binarySearchWithDuplicateSearchValues(arr []int, search int) (int, bool) {
+	if len(arr) == 0 {
+		return -1, false
+	}
+
+	l, r := 0, len(arr)-1
+
+	for l <= r {
+		mid := (l + r) / 2
+		if search <= arr[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return l, true
+}
+
+func binarySearchWithDuplicateSearchValues2(arr []int, search int) (int, bool) {
+	if len(arr) == 0 {
+		return -1, false
+	}
+
+	l, r := 0, len(arr)-1
+
+	for l <= r {
+		mid := (l + r) / 2
+		if search < arr[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return r, true
 }
